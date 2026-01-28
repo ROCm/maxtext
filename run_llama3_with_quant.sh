251a5610b08953cda34504b5f23d4682bc5146f1
scxfjiang <xuefei.jiang@amd.com>
2026-01-28 12:09:36 -0600

    update

--- run_llama3_with_quant.sh
+++ run_llama3_with_quant.sh
@@ -1,5 +1,11 @@
 export JAX_ENABLE_COMPILATION_CACHE=0
 unset JAX_COMPILATION_CACHE_DIR
-export XLA_FLAGS="--xla_dump_to=./log/llama3/with_quant --xla_dump_hlo_as_text --xla_dump_hlo_as_dot --xla_dump_hlo_pass_re=spmd-partitioning --xla_gpu_enable_cublaslt=true --xla_gpu_enable_triton_gemm=false"
 
-python3 -m MaxText.train MaxText/configs/models/gpu/llama3_8b.yml quantization="fp8"
+mkdir -p ./log/llama3/with_quant
+
+export XLA_FLAGS="--xla_dump_to=./log/llama3/with_quant --xla_dump_hlo_as_text --xla_dump_hlo_as_dot --xla_dump_hlo_pass_re='spmd-partitioning|while-loop-all-reduce-code-motion' --xla_gpu_enable_cublaslt=true --xla_gpu_enable_triton_gemm=false"
+TF_CPP_MIN_LOG_LEVEL=0
+TF_CPP_MAX_VLOG_LEVEL=5
+export TF_CPP_VMODULE=while_loop_all_reduce_code_motion=5,spmd_partitioner=5
+
+python3 -m MaxText.train MaxText/configs/models/gpu/llama3_8b.yml quantization="fp8" > ./log/llama3/with_quant/run.log 2>&1
