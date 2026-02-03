229af4f2f5a7541e117b5287592835c97297c916
scxfjiang <xuefei.jiang@amd.com>
2026-02-03 09:30:15 -0600

    update

--- run_llama3_with_quant.sh
+++ run_llama3_with_quant.sh
@@ -5,10 +5,10 @@ mkdir -p ./log/llama3/with_quant
 mkdir -p ./log/llama3/with_quant/run_logs
 mkdir -p ./log/llama3/with_quant/rocprof
 
-export XLA_FLAGS="--xla_dump_to=./log/llama3/with_quant --xla_dump_hlo_as_text --xla_dump_hlo_as_dot --xla_dump_hlo_pass_re='spmd-partitioning|while-loop-all-reduce-code-motion' --xla_gpu_enable_cublaslt=true --xla_gpu_enable_triton_gemm=false"
-TF_CPP_MIN_LOG_LEVEL=0
-TF_CPP_MAX_VLOG_LEVEL=5
-export TF_CPP_VMODULE=while_loop_all_reduce_code_motion=5,spmd_partitioner=5
+export XLA_FLAGS="--xla_dump_to=./log/llama3/with_quant --xla_dump_hlo_as_text --xla_dump_hlo_as_dot --xla_gpu_enable_cublaslt=true --xla_gpu_enable_triton_gemm=false"
+# TF_CPP_MIN_LOG_LEVEL=0
+# TF_CPP_MAX_VLOG_LEVEL=5
+# export TF_CPP_VMODULE=while_loop_all_reduce_code_motion=5,spmd_partitioner=5
 
 # Use rocprof to trace GPU activities
 # Options:
