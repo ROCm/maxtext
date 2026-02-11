8eb5b6ed6038be42b6c1e46a13f0c85776549326
gulsumgudukbay <gudukbay@gmail.com>
2026-02-11 21:53:58 +0000

    llama3 8b reproducer

---
+++ run_llama3.sh
@@ -0,0 +1,25 @@
+export JAX_ENABLE_COMPILATION_CACHE=0
+unset JAX_COMPILATION_CACHE_DIR
+
+EXP_NAME="llama3_8b_fsdp"
+
+mkdir -p ./log/llama3/${EXP_NAME}
+mkdir -p ./log/llama3/${EXP_NAME}/hlo_dumps
+mkdir -p ./log/llama3/${EXP_NAME}/run_logs
+mkdir -p ./log/llama3/${EXP_NAME}/rocprof
+
+export XLA_FLAGS="--xla_dump_to=./log/llama3/${EXP_NAME}/hlo_dumps --xla_dump_hlo_as_text --xla_dump_hlo_as_dot --xla_gpu_enable_cublaslt=true --xla_gpu_enable_triton_gemm=false"
+# TF_CPP_MIN_LOG_LEVEL=0
+# TF_CPP_MAX_VLOG_LEVEL=5
+# export TF_CPP_VMODULE=while_loop_all_reduce_code_motion=5,spmd_partitioner=5
+
+# Use rocprof to trace GPU activities
+# Options:
+#   --hip-trace: trace HIP API and GPU kernel execution
+#   --sys-trace: trace both HIP and HSA levels (more detailed)
+#   -d: output directory for trace files
+#   --stats: generate statistics CSV files
+rocprofv3 -d ./log/llama3/${EXP_NAME}/rocprof --hip-trace --kernel-trace --rccl-trace --stats --output-format=pftrace \
+    -- python3 -m MaxText.train src/maxtext/configs/gpu/models/llama3_8b.yml > ./log/llama3/${EXP_NAME}/run_logs/run.log 2>&1
+
+# python3 -m MaxText.train src/maxtext/configs/gpu/models/llama3_8b.yml > ./log/llama3/${EXP_NAME}/run_logs/run.log 2>&1
